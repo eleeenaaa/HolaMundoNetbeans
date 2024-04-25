fe238b43a483bbb5cbd678bccf79d258f2ac39af
Elena Rabano <elena.rabano.perez@iesjulianmarias.es>
2024-04-25 17:01:24 +0200

    segundo  Commit del proyecto

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -15,7 +15,7 @@ public class JavaApplication3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Hola");
+        System.out.print("Hola, que tal, esoty modificando");
     }
     
 }
